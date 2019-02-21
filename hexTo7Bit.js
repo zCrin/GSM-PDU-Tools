be81c5e912b52fb2cff44aa14bcf28b919a23c0f
zCrin <antoine.nga@icloud.com>
2019-02-21 20:01:11 +0100

    Update hexTo7Bit.js

--- hexTo7Bit.js
+++ hexTo7Bit.js
@@ -2,7 +2,9 @@ function to7Bit(str){
   var l = str.length;
   var r = "";
 	for(var d=0;d<l;d++){
-		d = ((d+1)%8 == 0)? d++ : d;
+		if((d+1)%8 == 0){
+			d++;
+		}
 	  var c =(((str[d+1])?str[d+1].charCodeAt(0).toString(2).slice(- (d+1)%8) : "") + str[d].charCodeAt(0).toString(2));
     r += parseInt(c.slice(0,c.length - ((str[d+1])?  (d+1)%8 -1 : 1)) , 2).toString(16).toUpperCase();
 	}
